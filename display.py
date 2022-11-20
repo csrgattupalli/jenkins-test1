536d977377aee117f58a373508a2d2d8310acc5c
csrgattupalli <105880124+csrgattupalli@users.noreply.github.com>
2022-11-20 10:22:43 +0530

    Create display.py

---
+++ display.py
@@ -0,0 +1,17 @@
+print('Hiiii...!')
+a = 10
+b = 20
+c = a+b
+print('Addition of a and b is: c = ',c)
+print('This is a Jenkins Demo...!')
+print('Trying to understand Jenkins-Git CI')
+print('Completed')
+
+list1 = [10, 20, 30, 40, 50]
+rest_list1 = [x*2 for x in list1]
+print(rest_list1)
+
+list2 = [100, 200, 300, 400, 500]
+rest_list2 = [x*0.1 for x in list2]
+print(rest_list2)
+print('the list is completed')
